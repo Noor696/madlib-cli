d4b679e51cc36ecf6502ec2582bbac8cd7c0e7fc
Noor696 <eng.nooralhateeb@gmail.com>
2022-10-11 09:48:47 +0300

    not finish

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -1,3 +1,13 @@
 def welcomemsg():
-    print ("Mad Libs game contain short stories with many key words replaced with blanks. Beneath each blank is specified a category, such as noun, verb, place, celebrity, exclamation or part of the body. One player asks the other players, in turn, to contribute a word of the specified type for each blank, but without revealing the context for that word. Finally, the completed story is read aloud. The result is usually a sentence which is comical, surreal and takes on somewhat of a nonsensical tone.")
-    print ("**please answer the question and press enter**")
+
+    print ("""
+    **************************************************************************************
+    ** Mad Libs game contain short stories with many key words replaced with blanks.    **
+    ** Beneath each blank is specified a category, such as noun, verb, place, Adjective **
+    ** One player asks in turn, to contribute a word of the specified type for each     **
+    ** blank, but without revealing the context for that word.                          **
+    ** Finally, the completed story is read aloud. The result is usually a sentence     **
+    ** which is comical, surreal and takes on somewhat of a nonsensical tone.           **
+    ** _________________________________________________________________________________**
+    **              please answer the question and press enter                          **
+    """)
