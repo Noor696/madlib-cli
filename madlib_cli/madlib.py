512d5888daa076f85dcd1a3921541cb1706d9694
Noor696 <eng.nooralhateeb@gmail.com>
2022-10-17 12:58:12 +0300

    done lab

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -1,26 +1,74 @@
+import json
+import os
 # I need (madlibs template, User Input, Build the story)
 
-def welcomemsg():
+class Madlib_Story:
+    path="/home/noor/AdvPy-401/madlib-cli/assets"
+    def __init__(self, story_sentence , story_template ):
+        self.story_sentence = story_sentence
+        self.story_template = story_template
+        self.story_word = []
+        self.Story = None
+      
+    def welcomemsg():
 
-    print ("""
-    **************************************************************************************
-    ** Mad Libs game contain short stories with many key words replaced with blanks.    **
-    ** Beneath each blank is specified a category, such as noun, verb, place, Adjective **
-    ** One player asks in turn, to contribute a word of the specified type for each     **
-    ** blank, but without revealing the context for that word.                          **
-    ** Finally, the completed story is read aloud. The result is usually a sentence     **
-    ** which is comical, surreal and takes on somewhat of a nonsensical tone.           **
-    ** _________________________________________________________________________________**
-    **                     please provide the following words:                          **
-    """)
+        print ("""
+               **************************************************************************************
+               ** Mad Libs game contain short stories with many key words replaced with blanks.    **
+               ** Beneath each blank is specified a category, such as noun, verb, place, Adjective **
+               ** One player asks in turn, to contribute a word of the specified type for each     **
+               ** blank, but without revealing the context for that word.                          **
+               ** Finally, the completed story is read aloud. The result is usually a sentence     **
+               ** which is comical, surreal and takes on somewhat of a nonsensical tone.           **
+               ** _________________________________________________________________________________**
+               **                     please provide the following words:                          **
+               """)
 
-def get_words_from_user(story_words):
- 
-    input_words= []
-    for word in story_words:
-        user_input = input(word + " ")
-        input_words.append(user_input)
-    return input_words
+    @classmethod
+    def getStory_template(cls,name, path=None):
+        if not path:
+            path = cls.path
+        fpath = os.path.join(path, name)
+        # print(os.path.exists(fpath))
+        with open(fpath, "r") as f:
+            data = json.load(f)
+         # print(data)
+        user_Story = cls(**data)
+        return user_Story
 
-input_words = get_words_from_user(["Adjective" , "Adjective" , "A First Name", "Past Tense Verb", "A First Name" , "Adjective", "Adjective", "Plural Noun", "Large Animal", "Small Animal", "A Girl's Name", "Adjective", "Plural Noun" , "Adjective", "Plural Noun", "Number 1-50" , "First Name", "Number", "Plural Noun", "Number", "Plural Noun"])
-print(input_words)
+    def get_StoryWord_from_user(self):
+        print ("""
+               **************************************************************************************
+               ** Mad Libs game contain short stories with many key words replaced with blanks.    **
+               ** Beneath each blank is specified a category, such as noun, verb, place, Adjective **
+               ** One player asks in turn, to contribute a word of the specified type for each     **
+               ** blank, but without revealing the context for that word.                          **
+               ** Finally, the completed story is read aloud. The result is usually a sentence     **
+               ** which is comical, surreal and takes on somewhat of a nonsensical tone.           **
+               ** _________________________________________________________________________________**
+               **                     please provide the following words:                          **
+               """)
+        
+        for word in self.story_sentence:
+          input_sentence = input(word + " ")
+          self.story_word.append(input_sentence)
+        return self.story_word
+
+    def build_Story(self):
+
+        self.Story = self.story_template.format(*self.story_word)
+        return (self.Story)
+
+    def show_Story(self):
+        with open("/home/noor/AdvPy-401/madlib-cli/assets/make_me_a_video_game_output.txt",'w') as file:
+            file.write(Story)
+        print("\n")
+        print (Story)
+
+
+
+temp_name = "make_me_a_video_game_template.json"
+user_Story= Madlib_Story.getStory_template(temp_name)
+sentences = user_Story.get_StoryWord_from_user()
+Story = user_Story.build_Story()
+user_Story.show_Story()
